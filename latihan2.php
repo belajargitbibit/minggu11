ce7c6bef4affe07447af0a22bf3ab7f39eabe315
Bibit Kukuh Wicaksono <bibitkukuh00@gmail.com>
2022-11-28 17:08:03 +0700

    task7

--- latihan2.php
+++ latihan2.php
@@ -2,20 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Latihan Looping</title>
 </head>
 <body>
-    <h2>Membuat Perulangan Tabel</h2>
+    <h2>Membuat perulangan tabel</h2>
     <table border="1" cellpadding="10" cellspacing="0">
         <?php
-        for ($b = 1; $b <= 3; $b++) {
+        for ($b=1; $b<=10; $b++){
             echo "<tr>";
-            for ($k = 1; $k <= 5; $k++) {
+            for ($k=1; $k<=8; $k++){
                 echo "<td> $b,$k </td>";
             }
-            echo "</tr>";
+            echo "<tr>";
         }
         ?>
-    </table>
 </body>
 </html>
